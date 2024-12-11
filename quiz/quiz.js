7c06ab2c723c0d6767e78b108995a33041e965e1
Mwenecho <harawamwenecho@gmail.com>
2024-12-11 21:49:46 +0200

    add a little css

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,6 +1,4 @@
 let container = document.querySelector('.first');
-// let next_button = document.getElementById('nxt_btn');
-// let prev_button = document.getElementById('prev_btn');
 const qst_navigation = document.querySelector('.qst_nav')
 let sub_ans = document.querySelector('.sub_ans')
 let index = 0;
@@ -12,7 +10,7 @@ const questions = [
         answer_2: "Lilongwe",
         answer_3: "Balaka",
         answer_4: "Kasungu",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 2
     },
     {
@@ -21,7 +19,7 @@ const questions = [
         answer_2: "Rice",
         answer_3: "Chiponde",
         answer_4: "Sweet Potatoes",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 1
     },
     {
@@ -30,7 +28,7 @@ const questions = [
         answer_2: "English",
         answer_3: "Chichewa",
         answer_4: "French",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 3
     },
     {
@@ -39,7 +37,7 @@ const questions = [
         answer_2: "Kilimanjaro Mountain",
         answer_3: "Nyika Platue",
         answer_4: "Zomba Mountain",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 1
     },
     {
@@ -48,7 +46,7 @@ const questions = [
         answer_2: "John Chilembwe",
         answer_3: "Hastings Kamuzu Banda",
         answer_4: "Bingu Mutharika",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 3
     },
     {
@@ -57,7 +55,7 @@ const questions = [
         answer_2: "Dwangwa River",
         answer_3: "Bua River",
         answer_4: "Shire River",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 4
     },
     {
@@ -66,7 +64,7 @@ const questions = [
         answer_2: "2024",
         answer_3: "1964",
         answer_4: "1891",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 3
     },
     {
@@ -75,7 +73,7 @@ const questions = [
         answer_2: "US Dollar",
         answer_3: "Malawi Rand",
         answer_4: "Malawi Kwacha",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 4
     },
     {
@@ -83,8 +81,8 @@ const questions = [
         answer_1: "Lake Malawi",
         answer_2: "Lake Tanzania-Malawi",
         answer_3: "Lake Shire",
-        answer_4: "2Lake Nyasa",
-        sumbitted_ans: "none",
+        answer_4: "Lake Nyasa",
+        sumbitted_ans: ".....",
         correct_ans: 1
     },
     {
@@ -93,7 +91,7 @@ const questions = [
         answer_2: "Africa",
         answer_3: "South America",
         answer_4: "Australia",
-        sumbitted_ans: "none",
+        sumbitted_ans: ".....",
         correct_ans: 2
     }
 ];
@@ -105,13 +103,13 @@ function load_question(qst_num){
     const my_html = 
         `
             <div class="second">
-                <h1> ${questions[qst_num].question}</h1>
+                <h2> ${questions[qst_num].question}</h2>
                 <div class="ans_container">
                     <button class="ans_btn" onclick="check_ans(1)"> ${questions[qst_num].answer_1}</button>
                     <button class="ans_btn" onclick="check_ans(2)"> ${questions[qst_num].answer_2}</button>
                     <button class="ans_btn" onclick="check_ans(3)"> ${questions[qst_num].answer_3}</button>
                     <button class="ans_btn" onclick="check_ans(4)"> ${questions[qst_num].answer_4}</button>
-                    <div class="sub_ans"> Selected: ${questions[qst_num].sumbitted_ans}</div>
+                    <div class="sub_ans"> <b>Selected Answer</b>: ${questions[qst_num].sumbitted_ans}</div>
                 </div>
             </div>
 
