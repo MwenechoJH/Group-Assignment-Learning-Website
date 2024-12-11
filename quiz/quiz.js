a7fca2afeabab78e6cc8d4a556f557a0736c0850
Mwenecho <harawamwenecho@gmail.com>
2024-12-11 21:09:04 +0200

    add js and fixed bug

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,12 +1,13 @@
 let container = document.querySelector('.first');
-const next_button = document.getElementById('nxt_btn');
-const prev_button = document.getElementById('prev_btn');
+// let next_button = document.getElementById('nxt_btn');
+// let prev_button = document.getElementById('prev_btn');
+const qst_navigation = document.querySelector('.qst_nav')
 let sub_ans = document.querySelector('.sub_ans')
 let index = 0;
 let score = 0;
 const questions = [
     {
-        question: "What is the capital of Malawi",
+        question: "What is the capital of Malawi?",
         answer_1: "Blantyre",
         answer_2: "Lilongwe",
         answer_3: "Balaka",
@@ -15,101 +16,144 @@ const questions = [
         correct_ans: 2
     },
     {
-        question: "What is your name?",
-        answer_1: "1",
-        answer_2: "1",
-        answer_3: "1",
-        answer_4: "1",
-        sumbitted_ans: "none",
-        correct_ans: 3
-    },
-    {
-        question: "Is malawi a cuountry?",
-        answer_1: "2",
-        answer_2: "2",
-        answer_3: "2",
-        answer_4: "2",
-        sumbitted_ans: "none",
-        correct_ans: 2
-    },
-    {
-        question: "What is your age?",
-        answer_1: "1",
-        answer_2: "32",
-        answer_3: "16",
-        answer_4: "2",
+        question: "What is the staple food in Malawi?",
+        answer_1: "Nsima",
+        answer_2: "Rice",
+        answer_3: "Chiponde",
+        answer_4: "Sweet Potatoes",
         sumbitted_ans: "none",
         correct_ans: 1
     },
     {
-        question: "What is your name?",
-        answer_1: "1",
-        answer_2: "1",
-        answer_3: "1",
-        answer_4: "1",
+        question: "What is the Mother language of Malawians?",
+        answer_1: "Chitumbuka",
+        answer_2: "English",
+        answer_3: "Chichewa",
+        answer_4: "French",
         sumbitted_ans: "none",
         correct_ans: 3
     },
     {
-        question: "Is malawi a cuountry?",
-        answer_1: "2",
-        answer_2: "2",
-        answer_3: "2",
-        answer_4: "2",
-        sumbitted_ans: "none",
-        correct_ans: 2
-    },
-    {
-        question: "What is your age?",
-        answer_1: "1",
-        answer_2: "32",
-        answer_3: "16",
-        answer_4: "2",
+        question: "What is the tallest mountain in Malawi?",
+        answer_1: "Mulanje Mountain",
+        answer_2: "Kilimanjaro Mountain",
+        answer_3: "Nyika Platue",
+        answer_4: "Zomba Mountain",
         sumbitted_ans: "none",
         correct_ans: 1
     },
     {
-        question: "What is your name?",
-        answer_1: "1",
-        answer_2: "1",
-        answer_3: "1",
-        answer_4: "1",
+        question: "What was the first president of Malawi?",
+        answer_1: "Lazarus Chakwera",
+        answer_2: "John Chilembwe",
+        answer_3: "Hastings Kamuzu Banda",
+        answer_4: "Bingu Mutharika",
         sumbitted_ans: "none",
         correct_ans: 3
     },
     {
-        question: "Is malawi a cuountry?",
-        answer_1: "2",
-        answer_2: "2",
-        answer_3: "2",
-        answer_4: "2",
+        question: "Which is the longest river in Malawi?",
+        answer_1: "Nyasa River",
+        answer_2: "Dwangwa River",
+        answer_3: "Bua River",
+        answer_4: "Shire River",
         sumbitted_ans: "none",
-        correct_ans: 2
+        correct_ans: 4
     },
     {
-        question: "Is malawi a cuountry?",
-        answer_1: "2",
-        answer_2: "2",
-        answer_3: "2",
-        answer_4: "2",
+        question: "In which year did Malawi get independece?",
+        answer_1: "1988",
+        answer_2: "2024",
+        answer_3: "1964",
+        answer_4: "1891",
+        sumbitted_ans: "none",
+        correct_ans: 3
+    },
+    {
+        question: "What is the currency of Malawi?",
+        answer_1: "Malawi Dollar",
+        answer_2: "US Dollar",
+        answer_3: "Malawi Rand",
+        answer_4: "Malawi Kwacha",
+        sumbitted_ans: "none",
+        correct_ans: 4
+    },
+    {
+        question: "What is the name of Malawi's Lake?",
+        answer_1: "Lake Malawi",
+        answer_2: "Lake Tanzania-Malawi",
+        answer_3: "Lake Shire",
+        answer_4: "2Lake Nyasa",
+        sumbitted_ans: "none",
+        correct_ans: 1
+    },
+    {
+        question: "In whick continent is Malawi Found?",
+        answer_1: "Asia",
+        answer_2: "Africa",
+        answer_3: "South America",
+        answer_4: "Australia",
         sumbitted_ans: "none",
         correct_ans: 2
     }
 ];
 
-const score_array = questions.map(x => 0 );
+let score_array = questions.map(x => 0 );
 
 // score_array[0] = 1;
 
+// function btn_state(value){
+//     console.log(value);
+//     if (value == 0){
+//         console.log('equal to zero');
+//         prev_button.remove()
+//         next_button.style.display = 'visible';
+        
+//      }
+//      else if (0<value && value < score_array.length -1){
+//         console.log('in between');
+//         prev_button.style.display = 'visible';
+//         next_button.style.display = 'visible';
+//     }
+//      else{
+//         console.log('maximum reached');
+//         prev_button.style.display = 'visible';
+//         next_button.style.display = 'visible';
+// }
+
+//     add_events();
+// }
 
 
-function sum(){
-    sum = 0
-    for(i of score_array){
-        sum += i;
+function edit_qst_nav(){
+    if(index == 0){
+        qst_navigation.innerHTML = 
+        `
+        <button id="nxt_btn"> Next</button>
+        `
+        
+    } else if( index == score_array.length-1){
+        qst_navigation.innerHTML = 
+        `
+        <button id="prev_btn">Previous</button>
+        <button id="sub_btn">Submit</button>
+
+        `
+        let sub_button = document.getElementById('sub_btn');
+        sub_button.addEventListener('click', sub_ans);
+    } else{
+        qst_navigation.innerHTML = 
+        `
+        <button id="prev_btn">Previous</button>
+        <button id="nxt_btn"> Next</button>
+        `
     }
-    score = sum;
+    
+    add_events();
+        
+    
 }
+
 
 function load_question(qst_num){
     const my_html = 
@@ -122,15 +166,18 @@ function load_question(qst_num){
                     <button class="ans_btn" onclick="check_ans(3)"> ${questions[qst_num].answer_3}</button>
                     <button class="ans_btn" onclick="check_ans(4)"> ${questions[qst_num].answer_4}</button>
                     <div class="sub_ans"> Selected: ${questions[qst_num].sumbitted_ans}</div>
-
                 </div>
             </div>
 
         `
-        
+        // btn_state(index);
+
         container.innerHTML= my_html;
+        edit_qst_nav();
         
 }
+
+
 function check_ans(ans){
     switch(ans){
         case 1:
@@ -176,8 +223,29 @@ function prev_question(){
         alert("There's no previous question");
     }
 }
-next_button.addEventListener('click', next_question);
-prev_button.addEventListener('click', prev_question);
+
+
+function sub_ans(){
+    console.log('soething')
+}
+
+
+function add_events(){
+    let next_button = document.getElementById('nxt_btn');
+    let prev_button = document.getElementById('prev_btn');
+    next_button.addEventListener('click', next_question);
+    prev_button.addEventListener('click', prev_question);
+}
+
+
+function sum(){
+    sum = 0
+    for(i of score_array){
+        sum += i;
+    }
+    score = sum;
+}
+
+
 
 load_question(index);
-
